82b85accf68ac9346d2e9fdb7f0bad9454c8af18
LjubomirBubko <bubko.nikolov30@gmail.com>
2024-04-30 00:44:40 +0200

    Додаден фолдерот Lab2_IT_2024

---
+++ LAB_2/Lab2_IT_2024/Lab2_IT_2024/Controllers/EventController.cs
@@ -0,0 +1,110 @@
+﻿using Lab2_IT_2024.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2_IT_2024.Controllers
+{
+    public class EventController : Controller
+    {
+        private static List<EventModel> events = new List<EventModel>
+        {
+            new EventModel(){Id = 1, Name = "Завршено средно образование", Location = "Македонија"},
+            new EventModel(){Id = 2, Name = "Запросување", Location = "Албанија"},
+            new EventModel(){Id = 3, Name = "Започната работа во компанија", Location = "Германија"}
+        };
+        // GET: Event
+        public ActionResult ShowEvents()
+        {
+            return View(events);
+        }
+
+        public ActionResult AddNewEvent(EventModel newEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                int newId = events.Any() ? events.Max(e => e.Id) + 1 : 1;
+                newEvent.Id = newId;
+                events.Add(newEvent);
+                return RedirectToAction("EventDetails", new { id = newId });
+            }
+
+            else
+            {
+                return View(newEvent);
+            }
+        }
+
+        public ActionResult EventDetails(string name)
+        {
+            var model = events.LastOrDefault();
+            return View(model);
+        }
+
+        public ActionResult EditEvent(int id)
+        {
+            EventModel eventToEdit = events.LastOrDefault(e => e.Id == id);
+
+            if (eventToEdit == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(eventToEdit);
+        }
+
+        [HttpPost]
+        public ActionResult EditEvent(EventModel eventToEdit)
+        {
+            if (ModelState.IsValid)
+            {
+                int index = events.FindIndex(e => e.Id == eventToEdit.Id);
+
+                if (index == -1)
+                {
+                    return HttpNotFound();
+                }
+
+                events[index] = eventToEdit;
+
+                return RedirectToAction("ShowEvents");
+            }
+
+            return View(eventToEdit);
+        }
+
+        public ActionResult DeleteEvent(int id)
+        {
+            EventModel eventToDelete = events.LastOrDefault(e => e.Id == id);
+
+            if (eventToDelete == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(eventToDelete);
+        }
+
+        [HttpPost, ActionName("DeleteEvent")]
+        public ActionResult ConfirmDeletion(int id)
+        {
+            int index = events.FindIndex(e => e.Id == id);
+
+            if (index == -1)
+            {
+                return HttpNotFound();
+            }
+
+            events.RemoveAt(index);
+
+            for (int i = 0; i < events.Count; i++)
+            {
+                events[i].Id = i + 1;
+            }
+
+            return RedirectToAction("ShowEvents");
+        }
+    }
+}
---
+++ LAB_2/Lab2_IT_2024/Lab2_IT_2024/Models/EventModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2_IT_2024.Models
+{
+    public class EventModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The event's name is required!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The event's location is required!")]
+        // Updated model for group B
+        [StringLength(30, MinimumLength = 5)]
+        public string Location { get; set; }
+
+        public static List<EventModel> Events { get; set; } = new List<EventModel>();
+    }
+}
